cff20601790bad0e8bcf2895f7969b2dac450aa4
daviresende03 <daviresendedev@gmail.com>
2022-07-04 21:17:47 -0300

    UserMap

---
+++ src/Api.Data/Mapping/UserMap.cs
@@ -0,0 +1,25 @@
+using Api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey( user => user.Id);
+
+            builder.HasIndex( user => user.Email)
+                .IsUnique();
+
+            builder.Property( user => user.Name)
+                .IsRequired()
+                .HasMaxLength(60);
+            
+            builder.Property( user => user.Email)
+                .HasMaxLength(100);
+        }
+    }
+}
