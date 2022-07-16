59fc1187765180162dd8ff7f43447b387e5ca301
daviresende03 <daviresendedev@gmail.com>
2022-07-16 10:13:44 -0300

    Swagger Implementation

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -30,6 +31,27 @@ namespace application
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Curso de Api com AspNetCore",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("https://github.com/daviresende03"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Davi",
+                        Email = "daviresendedev@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/daviresende03/")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso",
+                        Url = new Uri("https://github.com/daviresende03")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +61,12 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso API - DDD");    
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
